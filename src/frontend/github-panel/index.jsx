c49891ebcec3a84c0e27920a43862bcf593b9be7
Ruslan Ponomarenko <panruslan@gmail.com>
2025-08-26 16:48:02 +0300

    KAN-5: Create module and component to show PR

--- src/frontend/github-panel/index.jsx
+++ src/frontend/github-panel/index.jsx
@@ -1,19 +1,45 @@
-import ForgeReconciler, { Spinner, Text } from "@forge/react";
+import ForgeReconciler, { Button, Spinner, Stack } from "@forge/react";
 import React, { useEffect, useState } from "react";
 import { invoke } from "@forge/bridge";
+import { PullRequests } from "../app-settings/pull-requests";
+
+function resolvePrStatus(pr) {
+  if (pr.state === "open") {
+    return "OPEN";
+  }
+  if (pr.state === "closed" && !pr.merged_at) {
+    return "DECLINED";
+  }
+  if (pr.state === "closed" && pr.merged_at) {
+    return "MERGED";
+  }
+  return "UNKNOWN";
+}
+
+function mapPrs(prs) {
+  return prs.map((pr) => ({
+    id: pr.id,
+    name: pr.title,
+    status: resolvePrStatus(pr),
+    updatedAt: pr.updated_at,
+  }));
+}
 
 const GitHubPanel = () => {
   const [tokenExists, setTokenExists] = useState(undefined);
   const [prs, setPrs] = useState(undefined);
   useEffect(() => {
     invoke("gitHubTokenExists").then(setTokenExists);
-    invoke("getPrsForTicket").then((res) => setPrs(res.prs));
+    invoke("getPrsForTicket").then((res) => setPrs(mapPrs(res.prs)));
   }, []);
   if (tokenExists === undefined || prs === undefined) return <Spinner />;
+
   return (
-    <>
-      <Text>{prs.length}</Text>
-    </>
+    <Stack>
+      <Button appearance="subtle" isDisabled={prs.length === 0}>
+        <PullRequests prs={prs} />
+      </Button>
+    </Stack>
   );
 };
 ForgeReconciler.render(
